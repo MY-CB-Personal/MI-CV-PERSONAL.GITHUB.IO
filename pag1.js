4927e982599b655b9ae313dfd15f108991caa832
MY-CB-Personal <151438660+MY-CB-Personal@users.noreply.github.com>
2023-11-19 06:47:10 -0500

    Creando Nuestro Primer Sitio Web

---
+++ pag1.js
@@ -0,0 +1,10 @@
+// Puedes utilizar servicios como Google Maps para obtener un mapa interactivo
+
+// JavaScript para procesar el formulario
+function procesarFormulario() {
+    var nombre = document.getElementById('nombre').value;
+    var email = document.getElementById('email').value;
+  
+    // Aquí puedes realizar acciones con los datos, como enviarlos a un servidor
+    alert('Nombre: ' + nombre + '\nEmail: ' + email);
+  }  
